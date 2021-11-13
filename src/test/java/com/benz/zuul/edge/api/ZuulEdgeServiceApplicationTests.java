054159090809f81eeedf450b1ae7cf136b2cd9ee
Benz <Benzeman097-nafazbenzema@gmail.com>
2021-11-13 10:33:59 +0530

    api gateway initialized

---
+++ src/test/java/com/benz/zuul/edge/api/ZuulEdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benz.zuul.edge.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulEdgeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
