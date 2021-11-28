dce4dd44ff6987c088bb7ff265bb1e5f919a6ab6
Benz <Benzeman097-nafazbenzema@gmail.com>
2021-11-28 10:57:34 +0530

    docker file created for zuul-edge-service

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:11
-COPY ./build/libs/zuul-edge-service.jar.jar
+COPY ./build/libs/zuul-edge-service.jar zuul-edge-service.jar
 EXPOSE 8075
 CMD ["java","-jar","zuul-edge-service.jar"]
--- src/main/java/com/benz/zuul/edge/api/ZuulEdgeServiceApplication.java
+++ src/main/java/com/benz/zuul/edge/api/ZuulEdgeServiceApplication.java
@@ -3,10 +3,8 @@ package com.benz.zuul.edge.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy
 @EnableEurekaClient
 public class ZuulEdgeServiceApplication {
 
